58937f5347ddf8811c86fade4d4dabf0a71afb85
Furkan Aras KARİMAN <furkanaraskariman@gmail.com>
2026-09-25 19:01:35 +0300

    Add min and max functions to MainApp

--- MainApp.java
+++ MainApp.java
@@ -88,6 +88,27 @@ public class MainApp {
         //Average
         return sum / arr.length;
     }
+    //find the array's min
+    public static int findMin(int[] arr) {
+        int min = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        return min;
+    }
+
+    //find the array's max
+    public static int findMax(int[] arr) {
+        int max = arr[0];
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            }
+        }
+        return max;
+    }
     
     // Difference between number in array and average
     public static double[] findDifferences(int[] arr, double avg) {
